405829545bbb73983d17f0b8f24e597a3d16e686
Leonid Nikulin <leonk2020@gmail.com>
2019-12-04 22:00:25 +0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,66 @@
+const btn = document.querySelector('#root button');
+let pointer = 0;
+const answer = document.querySelector('#answer');
+let inputValue = '';
+
+btn.addEventListener('click', ()=>bracketString());
+
+function bracketString() {
+    try {
+        inputValue = document.querySelector('#input').value;
+        if (pointer >= inputValue.length) answer.innerHTML = 'Всё Правильно';
+        else {
+            nesting_lvl_1();
+            bracketString();
+            answer.innerHTML = 'Всё Правильно';
+        }
+    } catch (e) {
+        answer.innerHTML = e;
+    }
+    finally {
+        pointer = 0;
+    }
+}
+
+function nesting_lvl_1() {
+    let ch = inputValue[pointer];
+    if (ch === '(' || ch === '['){
+        if (ch === '(') {
+            ++pointer;
+            nesting_lvl_2();
+            ch = inputValue[pointer];
+            if (ch !== ')') throw Error(`Ожидался символ ) (ошибка на позиции ${pointer})`);
+        }
+        else if (ch === '[') {
+            ++pointer;
+            nesting_lvl_2();
+            ch = inputValue[pointer];
+            if (ch !== ']') throw Error(`Ожидался символ ] (ошибка на позиции ${pointer})`);
+        }
+        ++pointer;
+    }
+    else throw Error(`Ошибка ввода символа! (ошибка на позиции ${pointer})`);
+}
+
+function nesting_lvl_2() {
+    let ch = inputValue[pointer];
+    if (ch === '(' || ch === '['){
+        if (ch === '(') {
+            ch = inputValue[++pointer];
+            if (ch === ')'){
+                ++pointer;
+                nesting_lvl_2();
+            }
+            else throw Error(`Ожидался символ ) (ошибка на позиции ${pointer})`);
+        }
+        else if (ch === '[') {
+            ch = inputValue[++pointer];
+            if (ch === ']'){
+                ++pointer;
+                nesting_lvl_2();
+            }
+            else throw Error(`Ожидался символ ] (ошибка на позиции ${pointer})`);
+        }
+    }
+    if (ch===undefined) throw Error (`Не хватает символа!`);
+}
